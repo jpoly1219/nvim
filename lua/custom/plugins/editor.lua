3c74366380f57beb3aa31b3facecd6353742af12
Jacob Kim <jpoly1219@gmail.com>
2025-02-09 10:49:13 -0500

    add no neck pain

--- lua/custom/plugins/editor.lua
+++ lua/custom/plugins/editor.lua
@@ -32,7 +32,11 @@ return {
     'nvim-lualine/lualine.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     config = function()
-      require('lualine').setup {}
+      require('lualine').setup {
+        sections = {
+          lualine_c = { { 'filename', path = 1 } },
+        },
+      }
     end,
   },
   {
@@ -122,6 +126,7 @@ return {
         pickers = {
           find_files = {
             find_command = { 'rg', '--files', '--hidden', '--glob', '!**/.git/*' },
+            no_ignore = true,
           },
         },
         extensions = {
@@ -138,7 +143,8 @@ return {
               width = 0.8,
             },
             hijack_netrw = true,
-            hidden = { file_browser = true, folder_browser = false },
+            hidden = { file_browser = true, folder_browser = true },
+            no_ignore = { file_browser = true, folder_browser = true },
           },
         },
       }
@@ -245,5 +251,33 @@ return {
     -- config = function()
     --   require('lean').setup {}
     -- end,
+  },
+  {
+    'shortcuts/no-neck-pain.nvim',
+    version = '*',
+    opts = {
+      width = 120,
+      mappings = {
+        enabled = true,
+        toggle = '<leader>np',
+      },
+      autocmds = {
+        -- When `true`, enables the plugin when you start Neovim.
+        -- If the main window is  a side tree (e.g. NvimTree) or a dashboard, the command is delayed until it finds a valid window.
+        -- The command is cleaned once it has successfuly ran once.
+        ---@type boolean
+        enableOnVimEnter = true,
+        -- When `true`, enables the plugin when you enter a new Tab.
+        -- note: it does not trigger if you come back to an existing tab, to prevent unwanted interfer with user's decisions.
+        ---@type boolean
+        enableOnTabEnter = false,
+        -- When `true`, reloads the plugin configuration after a colorscheme change.
+        ---@type boolean
+        reloadOnColorSchemeChange = true,
+        -- When `true`, entering one of no-neck-pain side buffer will automatically skip it and go to the next available buffer.
+        ---@type boolean
+        skipEnteringNoNeckPainBuffer = false,
+      },
+    },
   },
 }
